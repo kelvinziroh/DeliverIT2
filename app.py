b5c81165d8f2308fb6fb90fa3578958f1e0b54f8
Kelvin Ziro <kelvin1@kelvins-MacBook-Pro.local>
2019-01-16 04:58:38 +0300

    Feature(Create parcel deliovery orders):
     - Add code for creating a parcel delivery order
    
    [Finishes #163220063]

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -26,6 +26,29 @@ parcels = [
 @app.route('/api/v1/parcels', methods=['GET'])
 def get_parcels():
     return jsonify({'parcels': parcels})
+
+@app.route('/api/v1/parcels/<int:parcel_id>', methods=['GET'])
+def get_parcel(parcel_id):
+    parcel = [parcel for parcel in parcels if parcel['id'] == parcel_id]
+    if len(parcel) == 0:
+        abort(404)
+    return jsonify({'parcel': parcel[0]})
+
+@app.route('/api/v1/parcels', methods=['POST'])
+def create_parcel():
+    if not request.json or not 'Sender\'s email address' in request.json:
+        abort(400)
+    parcel = {
+        'id': parcels[-1]['id'] + 1,
+        'Sender\'s email address': request.json['Sender\'s email address'],
+        'Receiver\'s email address': request.json['Receiver\'s email address'],
+        'Item': request.json['Item'],
+        'Pick-up location': request.json['Pick-up location'],
+        'Delivery Destination': request.json['Delivery Destination'],
+        'done': False
+    }
+    parcels.append(parcel)
+    return jsonify({'parcel': parcel }), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
